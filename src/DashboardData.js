bccdc9c8de0f5111d86a136f4a8e05dfc611187e
Kamil Nowak <it.kamilnowak@gmail.com>
2019-07-31 16:04:39 +0200

    AppBar and SideBar created

---
+++ src/DashboardData.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const DashboardData = () => <div>DashboardData</div>
+
+export default DashboardData
---
+++ src/DataField.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const DataField = () => <div>Field for data</div>
+
+export default DataField
