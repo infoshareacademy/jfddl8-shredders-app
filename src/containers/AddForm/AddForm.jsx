c11b282ac3940b3516e9bc72a29003e8047f70a7
Kamil Nowak <it.kamilnowak@gmail.com>
2019-08-05 16:58:54 +0200

    AddForm text fields added

--- src/containers/AddForm/AddForm.jsx
+++ src/containers/AddForm/AddForm.jsx
@@ -1,9 +1,65 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField'
 
-class AddForm extends React.Component{
-  render(){
-    return(
-      <div>AddForm</div>
+class AddForm extends React.Component {
+  state = {
+    formDate: {
+      brand: null,
+      date: null,
+      description: null,
+      location: null,
+      ticketPrice: null
+    }
+  }
+
+  changeHandler(evt, input) {
+    this.setState({
+      formDate: {
+        ...this.state.formDate,
+        [input]: evt.target.value
+      }
+    })
+  }
+
+  render() {
+    return (
+      <form noValidate autoComplete="off">
+        <TextField
+          id={'standard-name'}
+          label={'Brand'}
+          margin={'normal'}
+          fullWidth
+          onChange={evt => this.changeHandler(evt, 'brand')}
+        />
+        <TextField
+          id={'standard-name'}
+          label={'Date'}
+          margin={'normal'}
+          fullWidth
+          onChange={evt => this.changeHandler(evt, 'date')}
+        />
+        <TextField
+          id={'standard-name'}
+          label={'Description'}
+          margin={'normal'}
+          fullWidth
+          onChange={evt => this.changeHandler(evt, 'description')}
+        />
+        <TextField
+          id={'standard-name'}
+          label={'Location'}
+          margin={'normal'}
+          fullWidth
+          onChange={evt => this.changeHandler(evt, 'location')}
+        />
+        <TextField
+          id={'standard-name'}
+          label={'Ticket price'}
+          margin={'normal'}
+          fullWidth
+          onChange={evt => this.changeHandler(evt, 'ticketPrice')}
+        />
+      </form>
     )
   }
 }
