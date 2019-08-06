a9a080f57103402b054dac8bf189e9129ac0cfe2
Jakub Sawczuk <j.j.sawczuk@gmail.com>
2019-08-06 16:03:39 +0200

    first chart added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import { Route } from 'react-router-dom'
 
 import Router from './router/Router'
 import Nav from './router/Nav'
+import Chart from './Charts'
 
 const App = () => (
   <div>
+    
     <Router>
       <Nav />
     </Router>
+    <Chart></Chart>
   </div>
 )
+
+
 
 export default App
