4f23d26e767bc0595ac64999d58c46333180c901
Kamil Nowak <it.kamilnowak@gmail.com>
2019-08-07 23:57:05 +0200

    TextField as a separate component, PropTypes added

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 import { Button as MuiButton } from '@material-ui/core/';
 
 const Button = (props) => {
+  const { color, size, handleOnClick } = props
   return (
     <MuiButton
       variant={'contained'}
-      color={props.color}
-      fullWidth={props.fullWidth}
-      size={props.size}
-      onClick={props.onClick}
+      color={color}
+      fullWidth
+      size={size}
+      onClick={handleOnClick}
     >
       SUBMIT
     </MuiButton>
--- src/containers/AddForm/AddForm.jsx
+++ src/containers/AddForm/AddForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import TextField from '@material-ui/core/TextField'
+import TextField from '../../components/TextField'
 import Button from '../../components/Button';
 import { addConcertsToBase } from '../../services/fetchService';
 
@@ -9,6 +9,7 @@ class AddForm extends React.Component {
       band: '',
       date: '',
       description: '',
+      genre: '',
       location: '',
       ticketPrice: ''
     }
@@ -20,6 +21,7 @@ class AddForm extends React.Component {
         band: '',
         date: '',
         description: '',
+        genre: '',
         location: '',
         ticketPrice: ''
       }
@@ -41,9 +43,9 @@ class AddForm extends React.Component {
   }
 
   checkInputs = () => {
-    const { band, date, description, location, ticketPrice } = this.state.formDate
+    const { band, date, description, genre, location, ticketPrice } = this.state.formDate
 
-    return band.trim() && date.trim() && description.trim() && location.trim() && ticketPrice.trim()
+    return band.trim() && date.trim() && description.trim() && genre.trim() && location.trim() && ticketPrice.trim()
   }
 
   preventEmptyString = (evt) => {
@@ -69,56 +71,46 @@ class AddForm extends React.Component {
       <form noValidate autoComplete="off">
         <TextField
           value={this.state.formDate.band}
-          id={'standard-name'}
           label={'Band'}
-          margin={'normal'}
-          fullWidth
-          onChange={evt => this.changeHandler(evt, 'band')}
-          onKeyDown={this.onKeyDown}
+          changeHandler={evt => this.changeHandler(evt, 'band')}
+          handleKeyDown={this.onKeyDown}
         />
         <TextField
           value={this.state.formDate.date}
-          id={'standard-name'}
           label={'Date'}
-          margin={'normal'}
-          fullWidth
-          onChange={evt => this.changeHandler(evt, 'date')}
-          onKeyDown={this.onKeyDown}
+          changeHandler={evt => this.changeHandler(evt, 'date')}
+          handleKeyDown={this.onKeyDown}
         />
         <TextField
           value={this.state.formDate.description}
-          id={'standard-name'}
           label={'Description'}
-          margin={'normal'}
-          fullWidth
-          onChange={evt => this.changeHandler(evt, 'description')}
-          onKeyDown={this.onKeyDown}
+          changeHandler={evt => this.changeHandler(evt, 'description')}
+          handleKeyDown={this.onKeyDown}
+        />
+        <TextField
+          value={this.state.formDate.genre}
+          label={'Genre'}
+          changeHandler={evt => this.changeHandler(evt, 'genre')}
+          handleKeyDown={this.onKeyDown}
         />
         <TextField
           value={this.state.formDate.location}
-          id={'standard-name'}
           label={'Location'}
-          margin={'normal'}
-          fullWidth
-          onChange={evt => this.changeHandler(evt, 'location')}
-          onKeyDown={this.onKeyDown}
+          changeHandler={evt => this.changeHandler(evt, 'location')}
+          handleKeyDown={this.onKeyDown}
         />
         <TextField
           value={this.state.formDate.ticketPrice}
-          id={'standard-name'}
           label={'Ticket price'}
-          margin={'normal'}
-          fullWidth
-          onChange={evt => this.changeHandler(evt, 'ticketPrice')}
-          onKeyDown={this.onKeyDown}
+          changeHandler={evt => this.changeHandler(evt, 'ticketPrice')}
+          handleKeyDown={this.onKeyDown}
         />
         <Button
           color='primary'
-          fullWidth size='large'
-          onClick={this.preventEmptyString}
+          size='large'
+          handleOnClick={this.preventEmptyString}
         />
       </form>
-
     )
   }
 }
