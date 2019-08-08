e545f713554a32df24692b0c5113d2e5d5493018
Piotr Osoba <piotrosoba@outlook.com>
2019-08-08 17:00:53 +0200

    try to make class

--- src/components/List/ListItem/ListItem.js
+++ src/components/List/ListItem/ListItem.js
@@ -1,39 +1,128 @@
 import React from 'react'
 
-import MuiListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import ListItemText from '@material-ui/core/ListItemText';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
+import MuiListItem from '@material-ui/core/ListItem'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import Avatar from '@material-ui/core/Avatar'
+import ListItemText from '@material-ui/core/ListItemText'
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
 
-const ListItem = props => {
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+
+const styles = theme => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
+
+const DialogTitle = withStyles(styles)(props => {
+  const { children, classes, onClose } = props;
   return (
-    <div>
-      <MuiListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="concert" src="http://lorempixel.com/200/200" />
-        </ListItemAvatar>
-        <ListItemText
-          primary={props.data.band}
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                color="textPrimary"
-                style={{ display: 'inline' }}
-              >
-                {props.data.location}
-              </Typography>
-              {' — ' + props.data.date}
-            </React.Fragment>
-          }
-        />
-      </MuiListItem>
-      <Divider variant="inset" component="div" />
-    </div>
-  )
+    <MuiDialogTitle disableTypography className={classes.root}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles(theme => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles(theme => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
+
+const [open, setOpen] = React.useState(false);
+
+class ListItem extends React.Component {
+
+
+  handleClickOpen = () => {
+    setOpen(true);
+  };
+  handleClose = () => {
+    setOpen(false);
+  };
+
+
+  render() {
+    return (
+      <div>
+        <MuiListItem alignItems="flex-start" onClick={this.handleClickOpen}>
+          <ListItemAvatar>
+            <Avatar alt="concert" src="http://lorempixel.com/200/200" />
+          </ListItemAvatar>
+          <ListItemText
+            primary={this.props.data.band}
+            secondary={
+              <React.Fragment>
+                <Typography
+                  component="span"
+                  variant="body2"
+                  color="textPrimary"
+                  style={{ display: 'inline' }}
+                >
+                  {this.props.data.location}
+                </Typography>
+                {' — ' + this.props.data.date}
+              </React.Fragment>
+            }
+          />
+        </MuiListItem>
+        <Divider variant="inset" component="div" />
+
+        <Dialog onClose={this.handleClose} aria-labelledby="customized-dialog-title" open={open}>
+          <DialogTitle id="customized-dialog-title" onClose={this.handleClose}>
+            {this.props.data.band}
+          </DialogTitle>
+          <DialogContent dividers>
+            <Typography gutterBottom>
+              Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+              in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+          </Typography>
+            <Typography gutterBottom>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
+              lacus vel augue laoreet rutrum faucibus dolor auctor.
+          </Typography>
+            <Typography gutterBottom>
+              Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
+              scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
+              auctor fringilla.
+          </Typography>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="secondary">
+              Delete
+          </Button>
+          </DialogActions>
+        </Dialog>
+      </div >
+    )
+  }
 }
 
 export default ListItem
