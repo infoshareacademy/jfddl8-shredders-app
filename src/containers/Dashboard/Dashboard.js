04be9b2a8a25c983b198e3a1b0ea0499ebca610c
Piotr Osoba <piotrosoba@outlook.com>
2019-08-20 16:08:57 +0200

    added typhography component to dashboard

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -3,6 +3,8 @@ import React from 'react'
 import CustomersLoginsChart from '../Charts/CustomersLoginsChart'
 import CustomerSatisfactionChart from '../Charts/CustomerSatisfactionChart'
 
+import Typography from '@material-ui/core/Typography'
+
 const styles = {
   h2: {
     display: 'flex',
@@ -15,28 +17,29 @@ const styles = {
 
 const Dashboard = (props) => (
   <div>
-    <h2
+    <Typography variant='h4'
       style={
         styles.h2
       }>
-      MusicTripper - zaplanuj swoją muzyczną podróż </h2>
-    <div
+      MusicTripper - zaplanuj swoją muzyczną podróż </Typography>
+    <Typography
       style={
         styles.h2}
-    > MusicTripper jest aplikacją umożliwiającą zaplanowanie twojego koncertowego tripa. Jest to niezawodny towarzysz podróży, dzięki któremu wszystkie najpotrzebniejsze informacje będą w jednym miejscu. Odkrywaj najciekawsze wydażenia muzyczne w Twojej okolicy. Zabierz swoich znajomych w niezapomnianą muzyczną podróż!</div>
+    > MusicTripper jest aplikacją umożliwiającą zaplanowanie twojego koncertowego tripa. Jest to niezawodny towarzysz podróży, dzięki któremu wszystkie najpotrzebniejsze informacje będą w jednym miejscu. Odkrywaj najciekawsze wydażenia muzyczne w Twojej okolicy. Zabierz swoich znajomych w niezapomnianą muzyczną podróż!</Typography>
 
-    <p> W naszej aplikacji możesz zrobić następujące rzeczy:</p>
-    <ul>
-      <li>Przeglądać wszystkie koncerty w całej Polsce;</li>
-      <li>Dodawać interesujące Cię eventy do ulubionych;</li>
-    </ul>
-    <br />
-    <br />
-    <CustomerSatisfactionChart />
-    <br />
-    <br />
-    <CustomersLoginsChart />
-
+    <Typography> W naszej aplikacji możesz zrobić następujące rzeczy:</Typography>
+    <Typography variant={'subtitle1'}>
+      <ul>
+        <li>Przeglądać wszystkie koncerty w całej Polsce;</li>
+        <li>Dodawać interesujące Cię eventy do ulubionych;</li>
+      </ul>
+      <br />
+      <br />
+      <CustomerSatisfactionChart />
+      <br />
+      <br />
+      <CustomersLoginsChart />
+    </Typography>
   </div >
 )
 
