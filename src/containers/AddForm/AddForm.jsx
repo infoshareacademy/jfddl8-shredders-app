6d9cd3d563eada31e0d92abf619c2ff186d42f2c
Kamil Nowak <it.kamilnowak@gmail.com>
2019-08-20 15:20:57 +0200

    upload button added

--- src/containers/AddForm/AddForm.jsx
+++ src/containers/AddForm/AddForm.jsx
@@ -9,6 +9,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import { Paper } from '@material-ui/core'
 import MenuItem from '@material-ui/core/MenuItem';
 import MuiTextField from '@material-ui/core/TextField';
+import UploadButton from '../../components/UploadButton/UploadButton';
 
 class AddForm extends React.Component {
   state = {
@@ -175,6 +176,7 @@ class AddForm extends React.Component {
             handleOnClick={this.onSendData}
           />
         </form>
+        <UploadButton />
       </Paper>
     )
   }
