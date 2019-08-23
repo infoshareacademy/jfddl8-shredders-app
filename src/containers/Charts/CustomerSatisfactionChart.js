ea8c35fe8e841ed35c4ad51a72f0f56d26e91a66
Kamil Nowak <it.kamilnowak@gmail.com>
2019-08-23 17:02:24 +0200

    wip

--- src/containers/Charts/CustomerSatisfactionChart.js
+++ src/containers/Charts/CustomerSatisfactionChart.js
@@ -1,40 +1,77 @@
 import React, { Component } from 'react'
 
 import CanvasJSReact from '../../utlils/canvasjs.react'
+import withFetchService from '../../services/withFetchService'
+import { fetchs } from '../../state/concerts'
+import { isEqual, forEach } from 'lodash'
+
 const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class CustomerSatisfactionChart extends Component {
-	render() {
-		const options = {
-			animationEnabled: true,
-			title: {
-				text: "Eventy w naszej aplikacji"
-			},
-			subtitles: [{
-				verticalAlign: "center",
-				horizontalAlign: "center",
-				fontSize: 14,
-				dockInsidePlotArea: true
-			}],
-			data: [{
-				type: "doughnut",
-				showInLegend: true,
-				indexLabel: "{name}: {y}",
-				yValueFormatString: "#,###' %'",
-				dataPoints: [
-					{ name: " Festyny", y: 11 },
-					{ name: " Koncerty ", y: 50 },
-					{ name: " Festiwale ", y: 27 },
-					{ name: " Koncerty zagraniczne", y: 12 }
-				]
-			}]
-		}
-		return (
-			<div>
-				<CanvasJSChart options={options} />
-			</div>
-		);
-	}
+  state = {
+    concerts: {},
+    chartData: [
+      { name: " Festyny", y: 11 },
+      { name: " Koncerty ", y: 50 },
+      { name: " Festiwale ", y: 27 },
+      { name: " Koncerty zagraniczne", y: 12 }
+    ]
+  }
+
+  componentDidMount() {
+    this.props._getData()
+  }
+
+
+  setChartData(concerts) {
+    const countGenres = {}
+    forEach(concerts, (concert) => {
+      const genre = concert.genre.toLowerCase()
+      countGenres[genre] = countGenres[genre] ? countGenres[genre] + 1 : 1
+    })
+    const dataToChart = []
+    forEach(countGenres, (y, name) => {
+      dataToChart.push({ name, y })
+    })
+    this.setState({ chartData: dataToChart })
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+
+    if (!isEqual(nextProps._data, this.props._data)) {
+      this.setChartData(nextProps._data)
+    }
+  }
+
+  render() {
+    const options = {
+      animationEnabled: true,
+      title: {
+        text: "Eventy w naszej aplikacji"
+      },
+      subtitles: [{
+        verticalAlign: "center",
+        horizontalAlign: "center",
+        fontSize: 14,
+        dockInsidePlotArea: true
+      }],
+      data: [{
+        type: "doughnut",
+        showInLegend: true,
+        indexLabel: "{name}: {y}",
+        yValueFormatString: "#,###''",
+        dataPoints: this.state.chartData
+      }]
+    }
+    return (
+      <CanvasJSChart options={options} />
+    );
+  }
 }
 
-export default CustomerSatisfactionChart;   
+
+
+export default withFetchService(
+  'concerts',
+  fetchs
+)(CustomerSatisfactionChart);   
--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -2,41 +2,38 @@ import React from 'react'
 
 import CustomersLoginsChart from '../Charts/CustomersLoginsChart'
 import CustomerSatisfactionChart from '../Charts/CustomerSatisfactionChart'
+import { Typography } from '@material-ui/core';
 
 const styles = {
   h2: {
-    display: 'flex',
-    justifyContent: 'center'
+    textAlign: 'center',
+    margin: '50px auto',
+    maxWidth: 1000
   },
   div: {
-
+    margin: '50px',
+    display: 'flex',
+    justifyContent: 'center'
   }
 }
 
 const Dashboard = (props) => (
-  <div>
-    <h2
-      style={
-        styles.h2
-      }>
-      MusicTripper - zaplanuj swoją muzyczną podróż </h2>
-    <div
-      style={
-        styles.h2}
-    > MusicTripper jest aplikacją umożliwiającą zaplanowanie twojego koncertowego tripa. Jest to niezawodny towarzysz podróży, dzięki któremu wszystkie najpotrzebniejsze informacje będą w jednym miejscu. Odkrywaj najciekawsze wydażenia muzyczne w Twojej okolicy. Zabierz swoich znajomych w niezapomnianą muzyczną podróż!</div>
-
-    <p> W naszej aplikacji możesz zrobić następujące rzeczy:</p>
-    <ul>
-      <li>Przeglądać wszystkie koncerty w całej Polsce;</li>
-      <li>Dodawać interesujące Cię eventy do ulubionych;</li>
-    </ul>
-    <br />
-    <br />
-    <CustomerSatisfactionChart />
-    <br />
-    <br />
-    <CustomersLoginsChart />
-
+  <div style={{ display: 'flex', flexDirection: 'column', margin: 'auto' }}>
+    <div style={styles.h2}>
+      <Typography variant={'h2'} style={styles.h2}>
+        MusicTripper - zaplanuj swoją muzyczną podróż
+        </Typography>
+      <Typography>
+        MusicTripper jest aplikacją umożliwiającą zaplanowanie twojego koncertowego tripa. Jest to niezawodni towarzysz podróży, dzięki któremu wszystkie najpotrzebniejsze informacje będą w jednym miejscu. Odkrywaj najciekawsze wydażenia muzyczne w Twojej okolicy. Zabierz swoich znajomych w niezapomnianą muzyczną podróż!
+        </Typography>
+      <Typography> W naszej aplikacji możesz zrobić następujące rzeczy: </Typography>
+      <Typography>Przeglądać wszystkie koncerty w całej Polsce;</Typography>
+      <Typography>Dodawać interesujące Cię eventy do ulubionych;</Typography>
+    </div>
+    <div style={styles.div}>
+      <CustomerSatisfactionChart />
+      <CustomersLoginsChart />
+    </div>
   </div >
 )
 
